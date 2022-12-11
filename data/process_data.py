1b52c382f2df3a150379f4e641ab6e4fe0de3cee
Vu3003 <119339037+Vu3003@users.noreply.github.com>
2022-12-11 15:39:46 +0700

    finish code

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,36 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(messages, categories, on='id')
+    return df
 
 
 def clean_data(df):
-    pass
+    categories = df['categories'].str.split(';', expand=True)
+    row = categories.head(1)
+    category_colnames =  row.applymap(lambda x: x[:-2]).iloc[0,:]
+    categories.columns = category_colnames
+    for column in categories:
+        categories[column] = categories[column].str[-1].astype(int)
+
+    df.drop(columns = ['categories'], inplace=True)
+    df = pd.concat([df, categories], axis=1)
+    df.drop_duplicates(inplace=True)
+    return df
+
 
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine(f'sqlite:///{database_filename}')
+    df.to_sql('DataScience', engine, index=False, if_exists='replace')
+  
+
+
+
 
 
 def main():
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,66 @@
 import sys
+from sqlalchemy import create_engine
+import pickle
+import sqlite3
+import numpy as np
+import pandas as pd
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+from sklearn.metrics import classification_report
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
+
+nltk.download('punkt')
+nltk.download('wordnet')
 
 
 def load_data(database_filepath):
-    pass
+    engine = create_engine(f'sqlite:///{database_filepath}')
+    df = pd.read_sql_table('DataScience', con=engine)
+    X = df.message
+    y = df.iloc[:,4:]
+    category_names = list(df.columns[4:])
+    
+    return X, y, category_names
 
 
 def tokenize(text):
-    pass
+    tokens = word_tokenize(text)
+    lemma = WordNetLemmatizer()
+    clean_tokens = [lemma.lemmatize(token).lower().strip() for token in tokens]
+    return clean_tokens
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    parameters = {
+        'clf__estimator__n_estimators' : [10, 20, 30]
+    }
+    model = GridSearchCV(pipeline, param_grid=parameters)
+    return model
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    y_pred = model.predict(X_test)
+    for index, column in enumerate(category_names):
+        print(column, classification_report(Y_test[column], y_pred[:,index]))
+    
 
 
 def save_model(model, model_filepath):
-    pass
+    pickle.dump(model, open(model_filepath, 'wb'))
+
 
 
 def main():
